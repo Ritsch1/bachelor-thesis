3e2f57b6d928551fa37a11c7970f7e2ed93fb6dc
plesz <r.plesz@yahoo.de>
2021-08-22 00:10:15 +0200

    Added code to create Word-document-matrix.

--- code/python-code/WTMF.py
+++ code/python-code/WTMF.py
@@ -11,5 +11,13 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 get_ipython().system('jupyter nbconvert --output-dir="../python-code" --to python WTMF.ipynb --TemplateExporter.exclude_markdown=True --TemplateExporter.exclude_input_prompt=True')
 
 
-args = pd.read_csv("../../data/arguments.csv", sep=",", usecols=[""])
+# Read in argument-data
+args = pd.read_csv("../../data/arguments.csv", sep=",", usecols=["statement_id", "text_en"])
+# Convert to list for processing it further
+args = list(args["text_en"])
+
+
+# Exclude stop words while vectorizing the sentences
+vectorizer = TfidfVectorizer(stop_words="english")
+X = vectorizer.fit_transform(args)
 
