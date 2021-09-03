3b0a307e2a41c31240fcf81ed2d95a221d7920b9
plesz <r.plesz@yahoo.de>
2021-09-03 21:02:15 +0200

    Parametrize train function in main call to be able to set parameters externally.

--- code/python-code/WTMF.py
+++ code/python-code/WTMF.py
@@ -159,7 +159,7 @@ class WTMF():
 
 wtmf = WTMF(args)
 wtmf.create_tfidf_matrix()
-error = wtmf.train()
+error = wtmf.train(k=50, gamma=0.05, weight=0.05, training_iterations=50, random_seed=1, print_frequency=1)
 wtmf.compute_argument_similarity_matrix()
 wtmf.plot_training_error(error, title="WTMF Objective function error", xlabel="Iterations", ylabel="Error")
 
